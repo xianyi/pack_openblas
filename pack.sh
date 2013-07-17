2fcb543f5f746d3401a5edca65736a82c88388f3
Zhang Xianyi <traits.zhang@gmail.com>
2013-07-17 17:10:25 +0800

    Update the script.

--- pack.sh
+++ pack.sh
@@ -8,6 +8,9 @@ fi
 
 TOPDIR=`pwd`
 
+AWK=awk
+MD5SUM=md5sum
+
 GIT_LINK="github.com:xianyi/OpenBLAS.git"
 
 OS_list=(x86-Win x86_64-Win)
@@ -23,12 +26,29 @@ for (( i=0; i<$N; i++));  do
     cd ${OS_list[$i]}
     git clone $GIT_LINK .
     git checkout $BRANCH
-    make TARGET=NEHALEM DYNAMIC_ARCH=1 HOSTCC=gcc NUM_THREADS=64 ${COMMAND_list[$i]}
+
+#build
+    echo "make QUIET_MAKE=1 TARGET=NEHALEM DYNAMIC_ARCH=1 HOSTCC=gcc NUM_THREADS=64 ${COMMAND_list[$i]}" > build.log
+    make QUIET_MAKE=1 TARGET=NEHALEM DYNAMIC_ARCH=1 HOSTCC=gcc NUM_THREADS=64 ${COMMAND_list[$i]} >> build.log
     make TARGET=NEHALEM DYNAMIC_ARCH=1 HOSTCC=gcc NUM_THREADS=64 ${COMMAND_list[$i]} PREFIX=$TOPDIR/OpenBLAS-${BRANCH}-${OS_list[$i]} install
-    cd ..
+    cp exports/libopenblas.def $TOPDIR/OpenBLAS-${BRANCH}-${OS_list[$i]}/lib
+    cp build.log $TOPDIR/OpenBLAS-${BRANCH}-${OS_list[$i]}/readme.txt
+    cd -
+
+#mv libopenblas_xxx.a libopenblas.a
     cd $TOPDIR/OpenBLAS-${BRANCH}-${OS_list[$i]}/lib
     cp libopenblas.a libopenblas.a_1
     rm -f *.a
     mv libopenblas.a_1 libopenblas.a
     cd -
+
+#check md5, copy libopenblas import export library.
+    # af68bc634292ca0c735c0d8f08d76d68
+    LIBOPENBLAS_DEF_MD5=`${MD5SUM} $TOPDIR/OpenBLAS-${BRANCH}-${OS_list[$i]}/lib/libopenblas.def | ${AWK} '{print $1}'`
+    cp ./data/${LIBOPENBLAS_DEF_MD5}/${OS_list[$i]}/libopenblas.* OpenBLAS-${BRANCH}-${OS_list[$i]}/lib/
+
+    cat ./data/template.txt >> $TOPDIR/OpenBLAS-${BRANCH}-${OS_list[$i]}/readme.txt
+#package
+    zip -r OpenBLAS-${BRANCH}-${OS_list[$i]}.zip OpenBLAS-${BRANCH}-${OS_list[$i]}/
+
 done
